1c83eec09af12477bb79d8bc49d1037ab9a11ccb
AJITH P MOHAN <ajithpmohan90@gmail.com>
2021-08-24 19:27:57 +0530

    Docker image changed

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,9 +4,7 @@ if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
+    bash -c 'while !</dev/tcp/$SQL_HOST/$SQL_PORT; do sleep 1; done;'
 
     echo "PostgreSQL started"
 fi
